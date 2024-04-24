37a8d6129852ed9b141a2de1de855f72fb61bd0f
kelvintechsolutions <kevohmutugi51@gmail.com>
2024-04-24 05:11:10 -0700

    task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
-# Install the puppet package
-
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+# Install puppet-lint
+package { 'puppet-lint':
+  ensure   => '2.5.0',
+  provider => 'gem'
 }
