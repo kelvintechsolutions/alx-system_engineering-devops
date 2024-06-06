d92d2ba53684bfbff7e7c1ae38934df26faf123d
kelvintechsolutions <kevohmutugi51@gmail.com>
2024-06-06 01:56:50 -0700

    corrected

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,22 +1,19 @@
 #!/usr/bin/python3
-
 """
-Model working with Reddit API
+Script that queries subscribers on a given Reddit subreddit.
 """
 
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """
-    function that queries the Reddit API and returns the number of
-    subscribers for a given subreddit
-    """
-    headers = {"User-Agent": 'u/DepartureNo8863'}
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    response = requests.get(url, headers=headers)
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         data = response.json()
-        return data.get('data').get('subscribers')
+        subscribers = data['data']['subscribers']
+        return subscribers
     else:
         return 0
